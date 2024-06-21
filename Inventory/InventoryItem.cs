a8abfc96c9dd6ed21c0488ebc36f0334b9e6d06f
Karanpreet Singh Karanpreet Singh <Karanpreetsingh3164@conestogac.on.ca>
2024-06-21 15:06:23 -0400

    Add project files.

---
+++ Inventory/InventoryItem.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory
+{
+    internal class InventoryItem
+    {
+        // Properties
+        public string ItemName { get; set; }
+        public int ItemId { get; set; }
+        public double Price { get; set; }
+        public int QuantityInStock { get; set; }
+
+        // Constructor
+        public InventoryItem(string itemName, int itemId, double price, int quantityInStock)
+        {
+            // Initialize the properties with the values passed to the constructor
+            ItemName = itemName;
+            ItemId = itemId;
+            Price = price;
+            QuantityInStock = quantityInStock;
+        }
+
+        // Methods
+
+        // Update the price of the item
+        public void UpdatePrice(double newPrice)
+        {
+            // Update the item's price with the new price
+            Price = newPrice;
+        }
+
+        // Restock the item
+        public void RestockItem(int additionalQuantity)
+        {
+            // Increase the item's stock quantity by the additional quantity
+            QuantityInStock += additionalQuantity;
+        }
+
+        // Sell an item
+        public void SellItem(int quantitySold)
+        {
+            // Decrease the item's stock quantity by the quantity sold
+            // Make sure the stock doesn't go negative
+            if (QuantityInStock >= quantitySold)
+            {
+                QuantityInStock -= quantitySold;
+            }
+            else
+            {
+                Console.WriteLine("Not enough stock to sell.");
+            }
+        }
+
+        // Check if an item is in stock
+        public bool IsInStock()
+        {
+            // Return true if the item is in stock (quantity > 0), otherwise false
+            return QuantityInStock > 0;
+        }
+
+        // Print item details
+        public void PrintDetails()
+        {
+            // Print the details of the item (name, id, price, and stock quantity)
+            Console.WriteLine($"Item Name: {ItemName}");
+            Console.WriteLine($"Item ID: {ItemId}");
+            Console.WriteLine($"Price: {Price:C}");
+            Console.WriteLine($"Quantity in Stock: {QuantityInStock}");
+        }
+    }
+}
---
+++ Inventory/Program.cs
@@ -0,0 +1,35 @@
+﻿using Inventory;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating instances of InventoryItem
+        InventoryItem item1 = new InventoryItem("Laptop", 101, 1200.50, 10);
+        InventoryItem item2 = new InventoryItem("Smartphone", 102, 800.30, 15);
+
+        // Print details of all items
+        Console.WriteLine("Initial Inventory:");
+        item1.PrintDetails();
+        Console.WriteLine();
+        item2.PrintDetails();
+        Console.WriteLine();
+
+        // Sell some items and then print the updated details
+        Console.WriteLine("Selling 3 Laptops:");
+        item1.SellItem(3);
+        item1.PrintDetails();
+        Console.WriteLine();
+
+        // Restock an item and print the updated details
+        Console.WriteLine("Restocking 5 Smartphones:");
+        item2.RestockItem(5);
+        item2.PrintDetails();
+        Console.WriteLine();
+
+        // Check if an item is in stock and print a message accordingly
+        Console.WriteLine("Checking stock status:");
+        Console.WriteLine($"{item1.ItemName} is in stock: {item1.IsInStock()}");
+        Console.WriteLine($"{item2.ItemName} is in stock: {item2.IsInStock()}");
+    }
+}
